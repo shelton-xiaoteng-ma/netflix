0007a632cd5ee81084855d30ab8a28b14e2ea2d6
shelton <shelton.xiaoteng.ma@gmail.com>
2024-10-25 23:47:24 +0800

    prisma

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  namespace globalThis {
+    // eslint-disable-next-line no-var
+    var prismadb: PrismaClient = new PrismaClient();
+  }
+}
+export { };
+
