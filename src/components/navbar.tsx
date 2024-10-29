0f931aecb2086ce76613e2f3dd4ccd75b1e5134c
shelton <shelton.xiaoteng.ma@gmail.com>
2024-10-29 10:47:26 +0800

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -49,8 +49,6 @@ export const Navbar = () => {
           items-center
           transition
           duration-500
-          bg-zinc-900
-          bg-opacity-90
           ${showBackground ? "bg-zinc-900 bg-opacity-90" : ""}
         `}
       >
